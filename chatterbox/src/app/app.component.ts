e9cab716be552df0d7e1425129d274801c20e7b9
catstacks <cstanley390@gmail.com>
2023-06-25 19:16:20 +0100

    chat-dev: create .gitallowed, resolve promises

--- chatterbox/src/app/app.component.ts
+++ chatterbox/src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit {
     const apiKey = 'dz5f4d5kzrue';
     const userId = 'wandering-heart-0';
     const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoid2FuZGVyaW5nLWhlYXJ0LTAiLCJleHAiOjE2ODc3MjQwMzF9.hp-NozJwsJAtyBj8l2OinNwDaLG2J2SaFxIkiMoZa5U';
-    this.chatService.init(apiKey, userId, userToken);
-    this.streamI18nService.setTranslation();
+    this.chatService.init(apiKey, userId, userToken).then(() =>this.streamI18nService.setTranslation());
+
   }
 
   async ngOnInit() {
@@ -26,9 +26,9 @@ export class AppComponent implements OnInit {
       name: 'Talking about Angular',
     });
     await channel.create();
-    this.channelService.init({
+    await this.channelService.init({
       type: 'messaging',
-      id: { $eq: 'talking-about-angular' },
+      id: {$eq: 'talking-about-angular'},
     });
   }
 }
