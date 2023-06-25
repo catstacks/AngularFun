a9ef7d43797c41c7d2184f5b3b9ecfff5e0d7929
catstacks <cstanley390@gmail.com>
2023-06-25 18:41:17 +0100

    chat-dev: update app component

--- chatterbox/src/app/app.component.ts
+++ chatterbox/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatClientService, ChannelService, StreamI18nService } from 'stream-chat-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'chatterbox';
+export class AppComponent implements OnInit {
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'wandering-heart-0';
+    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoid2FuZGVyaW5nLWhlYXJ0LTAiLCJleHAiOjE2ODc3MjQwMzF9.hp-NozJwsJAtyBj8l2OinNwDaLG2J2SaFxIkiMoZa5U';
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
 }
